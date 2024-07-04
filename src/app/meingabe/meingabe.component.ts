2e45cc30e9559858d95599758283ce2d0f7134f0
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-07-04 20:54:50 +0200

    Form, Button, Karte eingespielt

--- src/app/meingabe/meingabe.component.ts
+++ src/app/meingabe/meingabe.component.ts
@@ -1,13 +1,30 @@
 import { Component } from "@angular/core";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { CommonModule } from "@angular/common";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: "app-meingabe",
+  selector: 'app-meingabe',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatFormFieldModule],
-  templateUrl: "./meingabe.component.html",
-  styleUrl: "./meingabe.component.scss",
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './meingabe.component.html',
+  styleUrl: './meingabe.component.scss',
 })
-export class MeingabeComponent {}
+export class MeingabeComponent {
+  inputControl = new FormControl('');
+  cardText: string = '';
+
+  updateCard() {
+    this.cardText = this.inputControl.value ?? '';
+  }
+}
