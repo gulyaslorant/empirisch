d5f42765b5ccf34c0fa695c3e481e47ebdb0b00a
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-07-14 21:24:02 +0200

    Anpassungen bei der Tabliste

--- src/app/tabliste/tabliste.component.ts
+++ src/app/tabliste/tabliste.component.ts
@@ -4,6 +4,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { GridGitterComponent } from '../gridgitter/gridgitter.component';
 import { FileTreeComponent } from '../file-tree/file-tree.component';
+import { Observable, Observer } from 'rxjs';
+
+interface ExampleTab {
+  label: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-tabliste',
@@ -20,6 +26,22 @@ import { FileTreeComponent } from '../file-tree/file-tree.component';
 })
 export class TablisteComponent {
   progressValue = 0;
+  asyncTabs: Observable<ExampleTab[]>;
+
+  constructor() {
+    this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
+      setTimeout(() => {
+        observer.next([
+          { label: 'Gridliste', content: 'Gridliste Content' },
+          {
+            label: 'Angular Dateistruktur',
+            content: 'Angular Dateistruktur Content',
+          },
+          { label: 'Tab 3', content: 'Tab 3 Content' },
+        ]);
+      }, 1000);
+    });
+  }
 
   onTabChange(index: number) {
     const tabCount = 3; // Anzahl der Tabs
