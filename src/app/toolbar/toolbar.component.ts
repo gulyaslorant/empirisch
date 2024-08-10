59d60669ccacc62593befb4418248c6dac8699b2
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-08-10 23:38:43 +0200

    Ausmisten

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,25 +1,23 @@
 import { Component } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
-import { SelectfieldComponent } from "../selectfield/selectfield.component";
-import { ExpansionPanelComponent } from "../expansion/expansion.component";
+import { SelectfieldComponent } from '../selectfield/selectfield.component';
 import { CommonModule } from "@angular/common"; // Wichtig für *ngIf und andere Common Features
 
 @Component({
-  selector: "app-toolbar",
+  selector: 'app-toolbar',
   standalone: true,
   imports: [
     MatToolbarModule,
     MatButtonModule,
     CommonModule,
     SelectfieldComponent,
-    ExpansionPanelComponent, // Um *ngIf zu unterstützen
   ],
-  templateUrl: "./toolbar.component.html",
-  styleUrls: ["./toolbar.component.scss"],
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent {
-  currentView: string = "home"; // Standardansicht auf 'home' setzen
+  currentView: string = 'home'; // Standardansicht auf 'home' setzen
 
   // Methode zum Setzen der aktuellen Ansicht
   setView(view: string): void {
