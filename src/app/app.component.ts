5cf7ea8774273cafe2670a369e08b8d55a39dfb3
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-08-10 23:44:34 +0200

    Loeschen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,38 +5,33 @@ import { SeitenleisteComponent } from "./seitenleiste/seitenleiste.component";
 import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule, ToolbarComponent],
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "Der Empirische Teil";
-  currentView: string = "home"; // Standardansicht auf 'home' setzen
+  title = 'Der Empirische Teil';
+  currentView: string = 'home'; // Standardansicht auf 'home' setzen
 
   // Explicitly define the type as Promise<Type<any>>
-  loadMenu: Promise<Type<any>> = import("./menupunkt/menupunkt.component").then(
-    (m) => m.MenupunktComponent,
-  );
+
   loadSeitenleiste: Promise<Type<any>> = import(
-    "./seitenleiste/seitenleiste.component"
+    './seitenleiste/seitenleiste.component'
   ).then((m) => m.SeitenleisteComponent);
-  loadToolbar: Promise<Type<any>> = import("./toolbar/toolbar.component").then(
-    (m) => m.ToolbarComponent,
+  loadToolbar: Promise<Type<any>> = import('./toolbar/toolbar.component').then(
+    (m) => m.ToolbarComponent
   );
   loadSelectfield: Promise<Type<any>> = import(
-    "./selectfield/selectfield.component"
+    './selectfield/selectfield.component'
   ).then((m) => m.SelectfieldComponent);
   loadSnackbar: Promise<Type<any>> = import(
-    "./snackbar/snackbar.component"
+    './snackbar/snackbar.component'
   ).then((m) => m.SnackbarComponent);
-  loadCheckbx: Promise<Type<any>> = import("./checkbx/checkbx.component").then(
-    (m) => m.CheckboxComponent,
+  loadCheckbx: Promise<Type<any>> = import('./checkbx/checkbx.component').then(
+    (m) => m.CheckboxComponent
   );
-  loadExpansion: Promise<Type<any>> = import(
-    "./expansion/expansion.component"
-  ).then((m) => m.ExpansionPanelComponent);
 
   ngOnInit() {}
 }
--- src/app/selectfield/selectfield.component.ts
+++ src/app/selectfield/selectfield.component.ts
@@ -5,15 +5,14 @@ import { CommonModule } from "@angular/common";
 import { TablisteComponent } from "../tabliste/tabliste.component";
 import { TablefeldComponent } from "../tablefeld/tablefeld.component";
 import { StepperComponent } from "../stepper/stepper.component";
-import { SeitenleisteComponent } from "../seitenleiste/seitenleiste.component";
-import { MenupunktComponent } from "../menupunkt/menupunkt.component";
-import { Observable, of, Subscription } from "rxjs";
+import { SeitenleisteComponent } from '../seitenleiste/seitenleiste.component';
+import { Observable, of, Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-selectfield",
+  selector: 'app-selectfield',
   standalone: true,
-  templateUrl: "./selectfield.component.html",
-  styleUrls: ["./selectfield.component.scss"],
+  templateUrl: './selectfield.component.html',
+  styleUrls: ['./selectfield.component.scss'],
   imports: [
     CommonModule,
     MatSelectModule,
@@ -22,26 +21,23 @@ import { Observable, of, Subscription } from "rxjs";
     TablefeldComponent,
     StepperComponent,
     SeitenleisteComponent,
-    MenupunktComponent,
   ],
 })
 export class SelectfieldComponent implements OnDestroy {
   options = [
-    { value: "tabliste", viewValue: "Tabliste" },
-    { value: "tablefeld", viewValue: "Tablefeld" },
-    { value: "stepper", viewValue: "Stepper Modul" },
+    { value: 'tabliste', viewValue: 'Tabliste' },
+    { value: 'tablefeld', viewValue: 'Tablefeld' },
+    { value: 'stepper', viewValue: 'Stepper Modul' },
   ];
 
-  selectedOption: string = "tablefeld";
+  selectedOption: string = 'tablefeld';
 
   loadSeitenleiste: Observable<typeof SeitenleisteComponent>;
-  loadMenupunkt: Observable<typeof MenupunktComponent>;
   private subscription: Subscription = new Subscription();
 
   constructor() {
     // SeitenleisteComponent und MenupunktComponent als Observable definieren
     this.loadSeitenleiste = of(SeitenleisteComponent);
-    this.loadMenupunkt = of(MenupunktComponent);
   }
 
   onSelectionChange(event: any) {
