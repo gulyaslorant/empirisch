d3db4145549c11c6a0324af8988c8f8237eac965
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-08-10 12:23:10 +0200

    Integration von Lazyload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, Type } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { SelectfieldComponent } from "./selectfield/selectfield.component";
-import { MenupunktComponent } from "./menupunkt/menupunkt.component";
-import { SeitenleisteComponent } from "./seitenleiste/seitenleiste.component";
-import { SnackbarComponent } from "./snackbar/snackbar.component";
+import { CommonModule } from "@angular/common";
+
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MenupunktComponent,
-    SeitenleisteComponent,
-    SelectfieldComponent,
-    SnackbarComponent,
-  ],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title = "Der Empirische Teil";
+
+  // Explicitly define the type as Promise<Type<any>>
+  loadSeitenleiste: Promise<Type<any>> = import(
+    "./seitenleiste/seitenleiste.component"
+  ).then((m) => m.SeitenleisteComponent);
+  loadMenupunkt: Promise<Type<any>> = import(
+    "./menupunkt/menupunkt.component"
+  ).then((m) => m.MenupunktComponent);
+  loadSelectfield: Promise<Type<any>> = import(
+    "./selectfield/selectfield.component"
+  ).then((m) => m.SelectfieldComponent);
+  loadSnackbar: Promise<Type<any>> = import(
+    "./snackbar/snackbar.component"
+  ).then((m) => m.SnackbarComponent);
 
   ngOnInit() {}
 }
